02854c6ad50afbbb4b142e55fe98ac9f1f4b8c99
CaptRojoThe3rd <115683287+CaptRojoThe3rd@users.noreply.github.com>
2022-12-19 21:02:52 -0800

    began work on title

--- compile.bat
+++ compile.bat
@@ -8,7 +8,7 @@ c:\cc65\bin\cc65 -Oirs game.c --add-source
 c:\cc65\bin\ca65 crt0.s
 c:\cc65\bin\ca65 game.s -g
 
-c:\cc65\bin\ld65 -C nrom_32k_vert.cfg -o connect_four.nes crt0.o game.o nes.lib -Ln labels.txt --dbgfile dbg.txt
+c:\cc65\bin\ld65 -C 17kb_rom.cfg -o connect_four.nes crt0.o game.o nes.lib -Ln labels.txt --dbgfile dbg.txt
 
 del *.o
 
